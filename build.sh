e90df8b3bf2e6769c39bff7295f391d5b7d0d027
tomtomcrypto <titiomm@gmail.com>
2022-11-05 12:16:45 +0000

    directory refactor

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ if [ ! -d "$PWD/build" ]
 then
   mkdir build
 fi
-eosio-cpp -I="./contracts/rng.oracle/include/" -R="./contracts/rng.oracle/resources" -o="./build/rng.oracle.wasm" -contract="rng.oracle" -abigen ./contracts/rng.oracle/src/rng.oracle.cpp
+eosio-cpp -I="./include/" -R="./resources" -o="./build/rng.oracle.wasm" -contract="rng.oracle" -abigen ./src/rng.oracle.cpp
